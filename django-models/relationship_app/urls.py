de434587da045715481a1210a69f8740ee4c3a25
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-11-06 09:54:30 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('book/add/', views.add_book, name='add_book'),
     path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
-    # Add other URL patterns here as needed
+    # Other URL patterns can be added here as needed
 ]
