3dbf23a61be61e4538b6c0717bcb6257fd5933b8
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-11-16 10:55:18 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)  # Date of birth field
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)  # Profile photo field
+    date_of_birth = models.DateField(null=True, blank=True)  # Optional date of birth field
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)  # Optional profile photo field
 
     def __str__(self):
         return self.username
