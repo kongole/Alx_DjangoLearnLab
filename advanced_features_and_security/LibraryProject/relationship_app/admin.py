235afcdbd82f2af5eb37690ba8bde56127140d25
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-11-16 10:57:21 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Library)
-admin.site.register(Librarian)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'date_of_birth']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'date_of_birth', 'profile_photo', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
 
+admin.site.register(CustomUser, CustomUserAdmin)
